c6bfa4360b2c23bdbc663a1053078091789d5887
Shree <shreekumarf@gmail.com>
2022-09-06 13:17:57 +0530

    initial commit

--- AWS_Bucket/main.tf
+++ AWS_Bucket/main.tf
@@ -1,9 +1,15 @@
-provider "aws"{
-    region = "ap-south-1"
+provider "aws" {
+  region = "ap-south-1"
 }
 resource "aws_s3_bucket" "shrees_bucket" {
-    bucket = "shrees-s3-bucket-first"  
-    versioning {
-      enabled = true
+  bucket = "shrees-s3-bucket-first"
+  # versioning {
+  #   enabled = true
+  # }
+  resource "aws_s3_bucket_versioning" "versioning_example" {
+    bucket = aws_s3_bucket.shrees_bucket.id
+    versioning_configuration {
+      status = "Enabled"
     }
+  }
 }
