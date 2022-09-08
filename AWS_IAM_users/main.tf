ec86c04217cff90955875fed58f745232f75bbae
Shree <shreekumarf@gmail.com>
2022-09-08 12:16:40 +0530

    initial commit

--- AWS_IAM_users/main.tf
+++ AWS_IAM_users/main.tf
@@ -1,8 +1,12 @@
+variable "iam_user_name_prefix" {
+  default = "my_iam_user"
+}
+
 provider "aws" {
   region = "ap-south-1"
 }
 
 resource "aws_iam_user" "my_iam_user" {
   count = 1
-  name  = "my_iam_user_${count.index}"
+  name  = "${var.iam_user_name_prefix}${count.index}"
 }
