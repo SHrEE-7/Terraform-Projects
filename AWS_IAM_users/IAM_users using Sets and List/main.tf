0c59144f5fe06fbc0f142e4420469e8f954911c7
Shree <shreekumarf@gmail.com>
2022-09-08 15:22:46 +0530

    initial commit

--- AWS_IAM_users/IAM_users using Sets and List/main.tf
+++ AWS_IAM_users/IAM_users using Sets and List/main.tf
@@ -1,11 +1,14 @@
 variable "names" {
-  default = ["jay","shri", "Ram"]
+    default = ["tom", "jack", "eric", "krish","shri"]
+#   default = ["jay","shri", "Ram"]
 }
 provider "aws" {
   region = "ap-south-1"
 }
 
 resource "aws_iam_user" "my_iam_users" {
-  count = length(var.names)
-  name = var.names[count.index]
+#   count = length(var.names)
+#   name = var.names[count.index]
+    for_each = toset(var.names)
+    name = each.value
 }
